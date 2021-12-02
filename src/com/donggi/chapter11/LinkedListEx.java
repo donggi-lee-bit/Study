1f69b1a361ebe1ad3d3d1f199b2727f8fa3df492
donggi-lee-bit <devdonggilee@gmail.com>
2021-12-03 04:14:01 +0900

    LinkedList 의 addAll() 메서드를 사용해보았다
    - addAll() 메서드를 사용하면 list 뒤에 매개변수로 전달된 컬렉션의 모든
      요소를 추가해준다!

---
+++ src/com/donggi/chapter11/LinkedListEx.java
@@ -0,0 +1,31 @@
+package com.donggi.chapter11;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListEx {
+
+    public static void main(String[] args) {
+        List<String> list = new LinkedList();
+
+        list.add("Geeks");
+        list.add("for");
+        list.add("Geeks");
+        list.add("10");
+        list.add("20");
+
+        Collection<String> collect = new ArrayList<String>();
+        collect.add("A");
+        collect.add("Computer");
+        collect.add("Portal");
+        collect.add("for");
+        collect.add("Geeks");
+
+        System.out.println("The LinkedList is: " + list);
+        list.addAll(collect);
+        System.out.println("The new Linked list is: " + list);
+
+    }
+}
