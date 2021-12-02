8747c26a38eac7762d644e6e82967ab8a2e18ad0
donggi-lee-bit <devdonggilee@gmail.com>
2021-12-02 22:37:44 +0900

    오버로딩에 대해 공부했다
    - 오버로딩은 메서드 이름이 같지만, 매개변수의 개수 또는 타입이 다른
      경우에 적용 가능하다
    - 반환 타입은 영향을 주지 않고 매개변수가 영향을 준다는 걸 유의해야
      한다.

--- src/com/donggi/chapter6/MyMathTest2.java
+++ src/com/donggi/chapter6/MyMathTest2.java
@@ -1,6 +1,6 @@
 package com.donggi.chapter6;
 
-public class MymathTest2 {
+public class MyMathTest2 {
     public static void main(String[] args) {
         System.out.println(MyMath2.add(200L, 100L)); // 클래스 메서드 호출
         MyMath2 mm = new MyMath2(); // 인스턴스 생성
