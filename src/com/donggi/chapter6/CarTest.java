16c973fb6ca9da490c747f43de2b7f369b815498
donggi-lee-bit <devdonggilee@gmail.com>
2021-12-02 23:55:05 +0900

    생성자에 대해 공부했다
    - 생성자는 인스턴스가 생성될 때마다 호출되는 '인스턴스 초기화
      메서드'이다.
    - 나는 생성자를 만들어 본 적이 없다. 그치만 클래스를 생성할 때
      컴파일러가 자동으로 기본생성자를 만들어준다고 한다
    - 일반적으로 만들어주는 게 맞다고 한다
    - 생성자도 오버로딩이 가능해서 매개 변수를 다르게 해주면 오버로딩을
      적용해서 같은 이름의 생성자를 여러개 사용할 수 있다
    - 생성자를 잘 사용하라 하였는데 이제 생성자에 대해 좀 알게 되었다

---
+++ src/com/donggi/chapter6/CarTest.java
@@ -0,0 +1,27 @@
+package com.donggi.chapter6;
+
+class Car {
+    String color;
+    String gearType;
+    int door;
+
+    Car() { // 기본 생성자
+
+    }
+
+    Car(String c, String g, int d) { // 매개변수가 있는 생성자
+        color = c;
+        gearType = g;
+        door = d;
+    }
+}
+
+public class CarTest {
+
+    public static void main(String[] args) {
+        Car c = new Car("white", "auto", 4); // 아래 세 줄을 여기 한 줄로 할 수 있다
+//        c.color = "white";
+//        c.gearType = "auto";
+//        c.door = 4;
+    }
+}
