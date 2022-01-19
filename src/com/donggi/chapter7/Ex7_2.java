c78fcd99f14ebd27568b6418dbc7884478ddc1e5
donggi-lee-bit <devdonggilee@gmail.com>
2022-01-20 00:18:48 +0900

    오버로딩 vs 오버라이딩
    - 오버로딩 오버라이딩에 공부하고 예제 코드를 따라 쳐보았다

--- src/com/donggi/chapter7/Ex7_2.java
+++ src/com/donggi/chapter7/Ex7_2.java
@@ -1,22 +1,63 @@
 package com.donggi.chapter7;
 
-class Parent {
-    int x = 10;
-}
-
-class Child extends Parent {
-    int x = 20;
-
-    void method() {
-        System.out.println("x = " + x);
-        System.out.println("this.x = " + this.x);
-        System.out.println("super.x = " + super.x);
-    }
-}
-
 public class Ex7_2 {
+
     public static void main(String[] args) {
         Child c = new Child();
         c.method();
+
+        System.out.println("==============");
+
+//        Point p = new Point();
+        Point3D p = new Point3D();
+        System.out.println(p.getLocation());
     }
 }
+
+class Parent {
+    int x = 10;
+
+    void parentMethod() {
+
+    }
+}
+
+class Child extends Parent{
+    int x = 20;
+
+    void parentMethod() {   // 오버라이딩
+
+    }
+
+    void parentMethod(int i) {  // 오버로딩
+
+    }
+
+    void childMethod() {}
+    void childMethod(int i) {}  // 오버로딩
+//    void childMethod() {} // -> 에러. 중복 정의.
+
+    void method() {
+       System.out.println("x=" + x);
+        System.out.println("this.x=" + this.x);
+        System.out.println("super.x=" + super.x);
+    }
+}
+
+class Point {
+    int x;
+    int y;
+
+    String getLocation() {
+        return "x :" + x + ", y :" + y;
+    }
+}
+
+class Point3D extends Point {
+    int z;
+    String getLocation() {
+        return super.getLocation() + ", z :" + z;
+    }
+}
+
+
