d7b2513731c71b7e39409bbacb17cbd0efcbc379
donggi-lee-bit <devdonggilee@gmail.com>
2022-01-30 23:08:31 +0900

    스트림 예제 Person 클래스 생성

---
+++ src/com/donggi/chapter14/스트림/Person.java
@@ -0,0 +1,13 @@
+package com.donggi.chapter14.스트림;
+
+public class Person {
+    private String name;
+    private int age;
+    private String phoneNumber;
+
+    public Person(String name, int age, String phoneNumber) {
+        this.name = name;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+    }
+}
