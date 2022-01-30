a7d916889d968b499042cb04bc25b9ad0d44c36b
donggi-lee-bit <devdonggilee@gmail.com>
2022-01-30 23:13:04 +0900

    Stream 예제

--- src/com/donggi/chapter14/스트림/Person.java
+++ src/com/donggi/chapter14/스트림/Person.java
@@ -1,5 +1,11 @@
 package com.donggi.chapter14.스트림;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class Person {
     private String name;
     private int age;
@@ -9,5 +15,28 @@ public class Person {
         this.name = name;
         this.age = age;
         this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public static void main(String[] args) {
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person("짱구", 23, "010-1234-1234"));
+        personList.add(new Person("유리", 24, "010-2341-1334"));
+        personList.add(new Person("철수", 29, "010-1414-5134"));
+        personList.add(new Person("맹구", 25, null));
+
+        Map<String, Person> personMap = personList.stream()
+                .collect(Collectors.toMap(Person::getName, Function.identity()));
     }
 }
